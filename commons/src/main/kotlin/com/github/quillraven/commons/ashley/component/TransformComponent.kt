a94c68a787960869c613b33e3aab5269b511b78c
Simon Klausner <quillraven@gmail.com>
2021-02-16 20:50:27 +0100

    add comments to TransformComponent

--- commons/src/main/kotlin/com/github/quillraven/commons/ashley/component/TransformComponent.kt
+++ commons/src/main/kotlin/com/github/quillraven/commons/ashley/component/TransformComponent.kt
@@ -2,6 +2,7 @@ package com.github.quillraven.commons.ashley.component
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.GdxRuntimeException
@@ -9,6 +10,18 @@ import com.badlogic.gdx.utils.Pool
 import ktx.ashley.get
 import ktx.ashley.mapperFor
 
+/**
+ * Component to store transformation related data.
+ *
+ * Use [position] to set the position of an [Entity]. The z coordinate is used for sorting the entities
+ * during render. If they are on the same layer (=same z value) then the y coordinate is used.
+ *
+ * Use [size] to set the width and height of an [Entity]. The values are in world units instead of pixels.
+ * The size also defines the size of the [Sprite] if an entity has a [RenderComponent].
+ *
+ * Use [transformCmp] to easily access the [TransformComponent] of an [Entity]. Only use it if you are sure that
+ * the component is not null. Otherwise, it will throw a [GdxRuntimeException].
+ */
 class TransformComponent : Component, Pool.Poolable, Comparable<TransformComponent> {
     val position = Vector3()
     val size = Vector2(1f, 1f)
@@ -28,6 +41,9 @@ class TransformComponent : Component, Pool.Poolable, Comparable<TransformCompone
     }
 }
 
+/**
+ * Returns a [TransformComponent] or throws a [GdxRuntimeException] if it doesn't exist.
+ */
 val Entity.transformCmp: TransformComponent
     get() = this[TransformComponent.MAPPER]
         ?: throw GdxRuntimeException("TransformComponent for entity '$this' is null")
