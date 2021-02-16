0c6a95131783e137f5be1d85cda330d5567975c0
Simon Klausner <quillraven@gmail.com>
2021-02-16 20:22:29 +0100

    add comments to PlayerComponent

--- commons/src/main/kotlin/com/github/quillraven/commons/ashley/component/PlayerComponent.kt
+++ commons/src/main/kotlin/com/github/quillraven/commons/ashley/component/PlayerComponent.kt
@@ -1,9 +1,15 @@
 package com.github.quillraven.commons.ashley.component
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.systems.IteratingSystem
 import com.badlogic.gdx.utils.Pool
 import ktx.ashley.mapperFor
 
+/**
+ * Component to identify a player [Entity]. Can be used for [iterating systems][IteratingSystem] to
+ * only run them for player entities.
+ */
 class PlayerComponent : Component, Pool.Poolable {
     override fun reset() = Unit
 
