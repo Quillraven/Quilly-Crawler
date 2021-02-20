83601c69cd6ffc92f15c6a9f1baafc48e8cd7c92
Simon Klausner <quillraven@gmail.com>
2021-02-20 21:28:58 +0100

    move entity creation by config to EntityConfigurations

--- commons/src/main/kotlin/com/github/quillraven/commons/ashley/EntityConfigurations.kt
+++ commons/src/main/kotlin/com/github/quillraven/commons/ashley/EntityConfigurations.kt
@@ -1,7 +1,19 @@
 package com.github.quillraven.commons.ashley
 
+import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.utils.GdxRuntimeException
 import com.badlogic.gdx.utils.ObjectMap
+import com.github.quillraven.commons.ashley.component.*
+import ktx.ashley.EngineEntity
+import ktx.ashley.entity
+import ktx.ashley.with
+import ktx.box2d.body
+import ktx.box2d.box
+import ktx.collections.contains
 import ktx.collections.set
 import ktx.log.debug
 import ktx.log.logger
@@ -24,7 +36,7 @@ internal constructor(
             callsInPlace(block, InvocationKind.AT_MOST_ONCE)
         }
 
-        if (configurations.containsKey(id)) {
+        if (id in configurations) {
             throw GdxRuntimeException("Configuration of id '$id' already exists. Configurations must be unique!")
         }
 
@@ -33,12 +45,69 @@ internal constructor(
         LOG.debug { "Adding configuration '$id': $newCfg" }
     }
 
-    operator fun get(id: String): AbstractEntityConfiguration {
+    operator fun get(id: String): ConfigType {
         return configurations[id] ?: throw GdxRuntimeException("There is no configuration for id '$id'")
     }
 
+    fun newEntity(
+        engine: Engine,
+        x: Float,
+        y: Float,
+        cfgId: String,
+        world: World? = null,
+        configure: EngineEntity.(ConfigType) -> Unit = {}
+    ): Entity {
+        val entityCfg = configurations[cfgId]
+
+        return engine.entity {
+            val transformCmp = with<TransformComponent> {
+                position.set(x, y, position.z)
+                size.set(entityCfg.size)
+            }
+
+            if (entityCfg.atlasFilePath.isNotBlank()) {
+                with<AnimationComponent> {
+                    atlasFilePath = entityCfg.atlasFilePath
+                    regionKey = entityCfg.regionKey
+                }
+                with<RenderComponent>()
+            }
+
+            if (world != null && entityCfg.bodyType != null) {
+                with<Box2DComponent> {
+                    body = world.body(BodyDef.BodyType.DynamicBody) {
+                        position.set(
+                            transformCmp.position.x + transformCmp.size.x * 0.5f,
+                            transformCmp.position.y + transformCmp.size.y * 0.5f
+                        )
+                        fixedRotation = true
+                        allowSleep = false
+                        val boundingBoxHeight = transformCmp.size.y * entityCfg.boundingBoxHeightPercentage
+                        box(
+                            transformCmp.size.x,
+                            boundingBoxHeight,
+                            Vector2(0f, -transformCmp.size.y * 0.5f + boundingBoxHeight * 0.5f)
+                        ) {
+                            friction = 0f
+                        }
+
+                        userData = this@entity.entity
+                    }
+                }
+            }
+
+            if (entityCfg.initialState != EntityState.EMPTY_STATE) {
+                with<StateComponent> {
+                    state = entityCfg.initialState
+                }
+            }
+
+            apply { configure(entityCfg) }
+        }
+    }
+
     companion object {
-        val LOG = logger<EntityConfigurations<*>>()
+        val LOG = logger<EntityConfigurations<AbstractEntityConfiguration>>()
 
         inline operator fun <reified ConfigType : AbstractEntityConfiguration> invoke(block: EntityConfigurations<ConfigType>.() -> Unit) =
             EntityConfigurations(ConfigType::class).apply(block)
--- core/src/main/kotlin/com/github/quillraven/quillycrawler/screen/PlayGroundScreen.kt
+++ core/src/main/kotlin/com/github/quillraven/quillycrawler/screen/PlayGroundScreen.kt
@@ -9,7 +9,6 @@ import com.badlogic.gdx.utils.viewport.FitViewport
 import com.github.quillraven.commons.ashley.component.PlayerComponent
 import com.github.quillraven.commons.ashley.component.box2dCmp
 import com.github.quillraven.commons.ashley.component.transformCmp
-import com.github.quillraven.commons.ashley.entityByCfg
 import com.github.quillraven.commons.ashley.system.*
 import com.github.quillraven.commons.game.AbstractScreen
 import com.github.quillraven.quillycrawler.QuillyCrawler
@@ -17,6 +16,7 @@ import com.github.quillraven.quillycrawler.ai.MessageType
 import com.github.quillraven.quillycrawler.ashley.EntityType
 import com.github.quillraven.quillycrawler.ashley.component.CollectableComponent
 import com.github.quillraven.quillycrawler.ashley.component.CollectingComponent
+import com.github.quillraven.quillycrawler.ashley.component.MoveComponent
 import com.github.quillraven.quillycrawler.ashley.component.PlayerControlComponent
 import com.github.quillraven.quillycrawler.ashley.system.CollisionSystem
 import com.github.quillraven.quillycrawler.ashley.system.MoveSystem
@@ -35,7 +35,6 @@ class PlayGroundScreen(
     private val box2DDebugRenderer = Box2DDebugRenderer()
     private val engine = PooledEngine().apply {
         addSystem(PlayerControlSystem(messageManager))
-        addSystem(EntityTypeStateAnimationSystem())
         addSystem(StateSystem(messageManager, MessageType.values().map { it.ordinal }.toSet()))
         addSystem(MoveSystem())
         addSystem(Box2DSystem(world, 1 / 60f))
@@ -54,24 +53,23 @@ class PlayGroundScreen(
     override fun show() {
         super.show()
         engine.run {
-            playerEntity()
+            game.entityConfigurations.newEntity(engine, 0f, 0f, EntityType.PLAYER.name, world) { cfg ->
+                with<PlayerComponent>()
+                with<PlayerControlComponent>()
+                with<CollectingComponent>()
+                this.entity.box2dCmp.body.circle(this.entity.transformCmp.size.x) {
+                    isSensor = true
+                }
+                with<MoveComponent> {
+                    maxSpeed = cfg.moveSpeed
+                }
+            }
 
-            entityByCfg(3f, 3f, EntityType.CHEST.name, game.entityConfigurations, world) {
+            game.entityConfigurations.newEntity(engine, 3f, 3f, EntityType.CHEST.name, world) {
                 with<CollectableComponent>()
             }
 
-            entityByCfg(1f, 0f, EntityType.BIG_DEMON.name, game.entityConfigurations)
-        }
-    }
-
-    private fun PooledEngine.playerEntity() {
-        entityByCfg(0f, 0f, EntityType.PLAYER.name, game.entityConfigurations, world) {
-            with<PlayerComponent>()
-            with<PlayerControlComponent>()
-            with<CollectingComponent>()
-            this.entity.box2dCmp.body.circle(this.entity.transformCmp.size.x) {
-                isSensor = true
-            }
+            game.entityConfigurations.newEntity(engine, 1f, 0f, EntityType.BIG_DEMON.name, world)
         }
     }
 
