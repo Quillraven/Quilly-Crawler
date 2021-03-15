9be224c36ccdb2f16d929a68ef0b23c5c0a3f39f
Simon Klausner <quillraven@gmail.com>
2021-03-15 20:59:57 +0100

    add ktx-actors dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
   implementation("com.badlogicgames.box2dlights:box2dlights:${project.property("box2dlightsVersion")}")
+  implementation("io.github.libktx:ktx-actors:${project.property("ktxVersion")}")
   implementation("io.github.libktx:ktx-scene2d:${project.property("ktxVersion")}")
   implementation("io.github.libktx:ktx-style:${project.property("ktxVersion")}")
   api(project(":commons"))
